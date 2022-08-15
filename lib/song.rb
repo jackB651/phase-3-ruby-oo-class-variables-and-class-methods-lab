0c6602321c5bc1a3d2ed6f80f8716c1c54e89e8b
Jonathon <jwbialas@gmail.com>
2022-08-14 21:09:59 -0500

    more finished

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,17 @@ require "pry"
 class Song
   attr_accessor :name, :artist, :genre
     @@count = 0
-    @@genre = []
+    @@genres = []
+    @@artists = []
   def initialize (name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count += 1
-    @@genre << genre
+    @@count = @@count + 1
+    @@genres << genre
+    @@artists << artist
   end
-  def count
+  def self.count
     @@count
   end
   def genre
